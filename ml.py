5bdc6e5a62a72fe4b970a0293f5f4949943faf8c
Amir-Taboul <amir.taboul@gmail.com>
2017-12-22 02:18:40 +0200

    ml

--- ml.py
+++ ml.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 
 def create_features(user, wanted_scenario='F'):
-    scenarios, actions, gains = user
+    scenarios, actions, gains, votes1, votes2, total_votes = user
     result_arr = []
     for index, vote_result in enumerate(izip(scenarios, actions, gains)):
         s, a, _ = vote_result
@@ -20,7 +20,11 @@ def create_features(user, wanted_scenario='F'):
 
             result_arr.append([mean_action_value,
                                np.mean(gains[:index] + gains[index + 1:]),
-                               a])
+                               np.mean([((v1 + v2) / t_v) for v1, v2, t_v in
+                                        izip(votes1, votes2, total_votes)]),
+                               float(votes1[index]) / total_votes[index],
+
+                               a]) # <- result
 
     return result_arr
 
@@ -35,18 +39,22 @@ if __name__ == "__main__":
     for ID, voter in grouped:
         user_arr.append(([s for s in voter['Scenario']],
                          [ac for ac in voter['Action']],
-                         [g for g in voter['CmpGain']]))
+                         [g for g in voter['CmpGain']],
+                         [v1 for v1 in voter['VotesCand1PreVote']],
+                         [v2 for v2 in voter['VotesCand2PreVote']],
+                         [v2 for v2 in voter['NumVotes']]))
 
     for _ in xrange(10):
         train, test = train_test_split(user_arr, test_size=0.2)
 
         train_set = reduce(lambda x, y: x + y, map(create_features, train))
         test_set = reduce(lambda x, y: x + y, map(create_features, test))
+        feature_number = len(train_set[0]) - 1
 
         svc = svm.SVC(kernel='linear')
-        svc.fit([x[:2] for x in train_set], [x[2] for x in train_set])
+        svc.fit([x[:feature_number] for x in train_set], [x[feature_number] for x in train_set])
 
-        result = svc.predict([x[:2] for x in test_set])
+        result = svc.predict([x[:feature_number] for x in test_set])
 
         print 'rate: {}'.format(float(
-            sum([1 for x, y in izip(test_set, result) if x[2] == y])) / len(result))
+            sum([1 for x, y in izip(test_set, result) if x[feature_number] == y])) / len(result))
